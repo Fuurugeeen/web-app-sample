e79f090354cc28739d838612ecd5b08372c91c52
Fuurugeeen <furugen07@gmail.com>
2025-02-02 23:03:58 +0900

    server.js にロギングミドルウェアを追加し、API エンドポイントのログ出力を削除

--- backend/server.js
+++ backend/server.js
@@ -12,13 +12,22 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
+const loggerMiddleware = function (req, res, next) {
+  console.log(`loggerMiddleware start ${req.method}:${req.url}`);
+  next();
+};
+app.use(loggerMiddleware);
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
 // データベース接続テスト
 app.get("/api", async (req, res) => {
   try {
-    console.log("api start");
     const result = await pool.query("SELECT NOW()");
     res.json({ message: "Hello from Express & Docker!", time: result.rows[0] });
-    console.log("api end");
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Database connection failed" });
@@ -27,10 +36,8 @@ app.get("/api", async (req, res) => {
 
 app.get("/api/users", async (req, res) => {
   try {
-    console.log("api get users start");
     const result = await pool.query("select * from users");
     res.json({ data: result.rowCount});
-    console.log("api get users end");
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Database connection failed" });
@@ -39,21 +46,15 @@ app.get("/api/users", async (req, res) => {
 
 app.post("/api/users", async (req, res) => {
   try {
-    console.log("api post users start");
     const { name } = req.body;
     const result = await pool.query(
       "INSERT INTO users (name) VALUES ($1) RETURNING *",
       [name]
     );
     res.json(result.rows[0]);
-    console.log("api post users end");
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Database connection failed" });
   }
 });
 
-const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
